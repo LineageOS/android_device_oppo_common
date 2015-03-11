5d8d0abf448d6335f9c5418840657e10c5f88b69
Roman Birg <roman@cyngn.com>
2015-03-11 15:06:26 -0700

    common: Add SELinux permissions for keyhandler
    Change-Id: Icaac8deb72f57ab4cab1538ca602c1c37959fe1b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,3 +28,7 @@ TARGET_POWERHAL_VARIANT := qcom
 # Releasetools
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_oppo
 TARGET_RELEASETOOLS_EXTENSIONS := device/oppo/common
+
+# SELinux policies
+BOARD_SEPOLICY_DIRS += \
+    device/oppo/common/sepolicy

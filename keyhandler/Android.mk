321482c6982837f9ec564380a0d9c75f55069cb0
Glen D'souza <glndsouza2691995@gmail.com>
2015-05-25 15:25:29 +0530

    oppo-common: Unbreak touchscreen gestures
    * Allow builds using WITH_DEXPREOPT to maintain screen-off gesture
      functionality
    
    Change-Id: I07b0a6cf611c16fe16b3627916dbce0b2f470896

--- keyhandler/Android.mk
+++ keyhandler/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := com.cyanogenmod.keyhandler
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 LOCAL_MODULE_TAGS := optional
+LOCAL_DEX_PREOPT := false
 
 include $(BUILD_JAVA_LIBRARY)
 

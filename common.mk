1152092e7096fa522522490c9976f6ff2d598abd
Roman Birg <roman@cyngn.com>
2015-06-02 16:18:22 -0700

    KeyHandler: never dexopt it
    Ever.
    
    Change-Id: Ib5bd2436c8ea5ed07a35fd78a239fc3448a63132
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit 41be3707b30366bbd113a04192a32e074ba8f8d9)

--- common.mk
+++ common.mk
@@ -24,6 +24,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
 
+# never dexopt the keyhandler
+$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
+
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_oppo

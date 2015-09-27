44bfe3ba12d8f66e744273adc0354fb6511894c0
Willi Ye <williye97@gmail.com>
2015-09-27 22:09:00 +0200

    configpanel: Add notification slider settings
    Change-Id: I392b1e14ae6bf9250763d73a2c5d27c90a58e472
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

---
+++ configpanel/src/com/cyanogenmod/settings/device/SliderSettings.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.device;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+
+import com.cyanogenmod.settings.device.utils.Constants;
+import com.cyanogenmod.settings.device.utils.FileUtils;
+import com.cyanogenmod.settings.device.utils.NodePreferenceActivity;
+
+import org.cyanogenmod.internal.util.ScreenType;
+
+public class SliderSettings extends NodePreferenceActivity {
+
+    private ListPreference mSliderTop;
+    private ListPreference mSliderMiddle;
+    private ListPreference mSliderBottom;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.slider_panel);
+
+        mSliderTop = (ListPreference) findPreference("keycode_slider_top");
+        setSummary(mSliderTop, Constants.KEYCODE_SLIDER_TOP);
+        mSliderTop.setOnPreferenceChangeListener(this);
+
+        mSliderMiddle = (ListPreference) findPreference("keycode_slider_middle");
+        setSummary(mSliderMiddle, Constants.KEYCODE_SLIDER_MIDDLE);
+        mSliderMiddle.setOnPreferenceChangeListener(this);
+
+        mSliderBottom = (ListPreference) findPreference("keycode_slider_bottom");
+        setSummary(mSliderBottom, Constants.KEYCODE_SLIDER_BOTTOM);
+        mSliderBottom.setOnPreferenceChangeListener(this);
+    }
+
+    private void setSummary(ListPreference preference, String file) {
+        String keyCode = FileUtils.readOneLine(file);
+        if (keyCode != null) {
+            preference.setValue(keyCode);
+            preference.setSummary(preference.getEntry());
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final String file;
+        if (preference == mSliderTop) {
+            file = Constants.KEYCODE_SLIDER_TOP;
+        } else if (preference == mSliderMiddle) {
+            file = Constants.KEYCODE_SLIDER_MIDDLE;
+        } else if (preference == mSliderBottom) {
+            file = Constants.KEYCODE_SLIDER_BOTTOM;
+        } else {
+            return false;
+        }
+
+        final String value = (String) newValue;
+        FileUtils.writeLine(file, value);
+        setSummary((ListPreference) preference, file);
+
+        Constants.savePreferenceInt(this, preference.getKey(), Integer.parseInt(value));
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // If running on a phone, remove padding around the listview
+        if (!ScreenType.isTablet(this)) {
+            getListView().setPadding(0, 0, 0, 0);
+        }
+    }
+}

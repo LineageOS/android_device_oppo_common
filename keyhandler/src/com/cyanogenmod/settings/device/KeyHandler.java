8f0509f1dbd8e0ceb6d4ad21f9b884b762c8017d
Gianmarco Reverberi <gianmarco.reverberi@gmail.com>
2015-10-25 10:06:59 +0100

    keyhandler: declare wakeup reason
    Change-Id: I910b8808e7037e84c6c61d2caf402645d05b713d

--- keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
+++ keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
@@ -187,7 +187,8 @@ public class KeyHandler implements DeviceKeyHandler {
         boolean isKeySupported = ArrayUtils.contains(sSupportedGestures, event.getScanCode());
         if (isKeySupported && !mEventHandler.hasMessages(GESTURE_REQUEST)) {
             if (event.getScanCode() == KEY_DOUBLE_TAP && !mPowerManager.isScreenOn()) {
-                mPowerManager.wakeUpWithProximityCheck(SystemClock.uptimeMillis());
+                mPowerManager.wakeUpWithProximityCheck(SystemClock.uptimeMillis(),
+                        "KeyHandlerWakeUp");
                 doHapticFeedback();
                 return true;
             }

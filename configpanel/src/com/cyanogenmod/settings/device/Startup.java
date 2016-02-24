f9cc420e7e8cff9fed108f9563739cb13ff5ffca
Roman Birg <roman@cyngn.com>
2016-02-24 08:59:05 -0800

    common: setup node prefs on cmhw initialization
    BOOT_COMPLETED wasn't fast enough to restore the settings by the time
    the user can interact with the device.
    
    Ref: OPO-513
    
    Change-Id: Ic701a1e884d3d8275ec07db904163b4492b44a2a
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit 7c014d65fa1d48e807c496d51f99b907ff5e36ed)

--- configpanel/src/com/cyanogenmod/settings/device/Startup.java
+++ configpanel/src/com/cyanogenmod/settings/device/Startup.java
@@ -52,8 +52,7 @@ public class Startup extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
         final String action = intent.getAction();
-        if (Intent.ACTION_BOOT_COMPLETED.equals(action)
-                || Intent.ACTION_PRE_BOOT_COMPLETED.equals(action)) {
+        if (cyanogenmod.content.Intent.ACTION_INITIALIZE_CM_HARDWARE.equals(action)) {
             // Disable touchscreen gesture settings if needed
             if (!hasTouchscreenGestures()) {
                 disableComponent(context, TouchscreenGestureSettings.class.getName());

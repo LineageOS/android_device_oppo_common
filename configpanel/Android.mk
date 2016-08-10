d0d86d6bf756d90824c923f3be9f02c8dc298975
Roman Birg <roman@cyngn.com>
2016-08-10 16:38:13 -0700

    ConfigPanel: add Settings search indexing
    Allows Settings to search for options provided by this package.
    
    Ticket: CYNGNOS-2014
    
    Change-Id: I7469c987267224b1b16ab324ab41bf5967b1cfa0
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -19,12 +19,12 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := ConfigPanel
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     org.cyanogenmod.platform.internal
 
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_PACKAGE)

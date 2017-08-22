10d9f2001db958c84b6f34185a173b80af814662
Simon Shields <simon@lineageos.org>
2017-08-23 00:50:14 +1000

    DO NOT MERGE: oppo: disable keyhandler
    Change-Id: I7f0d26af58334d39422813da95c235d8540ec5e9

--- common.mk
+++ common.mk
@@ -18,14 +18,14 @@
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Keyhandler
-PRODUCT_PACKAGES += \
-    ConfigPanel \
-    com.cyanogenmod.keyhandler
+#PRODUCT_PACKAGES += \
+#    ConfigPanel \
+#    com.cyanogenmod.keyhandler
 
-PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
+#PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
 
 # never dexopt the keyhandler
-$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
+#$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
 
 # Recovery
 PRODUCT_PACKAGES += \

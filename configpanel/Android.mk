5b964b29fde9b29f8191d73877b34e2fdd4805ec
Bruno Martins <bgcngm@gmail.com>
2017-10-06 00:08:00 +0200

    oppo: ConfigPanel: Re-integrate into Settings
     * Apply the default Oreo theme and inherit the layouts from Google
       for the Settings app, in order to keep UI consistency.
    
     * Add required appcompat and preference support libraries.
    
     * Integrate additional buttons into our custom Button settings
       category by defining the expected action.
    
     * Integrate O-Click into "Connected devices" and set empty summary
       to center.
    
    Change-Id: I79a227dcc94cc36f2ad7c825401439ba0f0de9ea
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -21,15 +21,19 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := ConfigPanel
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    android-support-v14-preference \
+    android-support-v7-appcompat \
+    android-support-v7-preference \
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
-
-LOCAL_AAPT_FLAGS := --auto-add-overlay
+    org.lineageos.platform.internal \
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    $(LOCAL_PATH)/../../../../packages/resources/devicesettings/res
+    $(TOP)/packages/resources/devicesettings/res \
 
 LOCAL_MODULE_TAGS := optional
 

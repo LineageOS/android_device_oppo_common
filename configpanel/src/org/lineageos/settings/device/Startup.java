dc0149aff74d11687b6ca8f07412d43825894c0f
Alexander Martinz <alex@amartinz.at>
2017-10-15 19:52:59 +0200

    oppo: ConfigPanel: remove gesture pad code
    Gesture code is not in frameworks anymore and gesture pad is only
    supported by OPPO N1 as well, which has no builds anymore.
    
    Kill the existing gesture pad code.
    
    Change-Id: If8cf71c84c6f8402d272e93702725f3311b87f8d
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- configpanel/src/org/lineageos/settings/device/Startup.java
+++ configpanel/src/org/lineageos/settings/device/Startup.java
@@ -26,20 +26,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.hardware.input.InputManager;
 import android.os.Build;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.os.ServiceManager;
-import android.os.SystemClock;
 import android.os.UserHandle;
 import android.preference.PreferenceManager;
-import android.service.gesture.IGestureService;
 import android.util.Log;
-import android.view.InputDevice;
-import android.view.InputEvent;
-import android.view.KeyCharacterMap;
-import android.view.KeyEvent;
 
 import org.lineageos.internal.util.FileUtils;
 import org.lineageos.settings.device.utils.Constants;
@@ -52,31 +45,6 @@ public class Startup extends BroadcastReceiver {
     public void onReceive(final Context context, final Intent intent) {
         final String action = intent.getAction();
         if (lineageos.content.Intent.ACTION_INITIALIZE_LINEAGE_HARDWARE.equals(action)) {
-            // Disable backtouch settings if needed
-            if (hasGestureService(context)) {
-                disableComponent(context, GesturePadSettings.class.getName());
-            } else {
-                IBinder b = ServiceManager.getService("gesture");
-                IGestureService sInstance = IGestureService.Stub.asInterface(b);
-
-                boolean value = Constants.isPreferenceEnabled(context,
-                        Constants.TOUCHPAD_STATE_KEY);
-                String node = Constants.sBooleanNodePreferenceMap.get(
-                        Constants.TOUCHPAD_STATE_KEY);
-                if (!FileUtils.writeLine(node, value ? "1" : "0")) {
-                    Log.w(TAG, "Write to node " + node +
-                            " failed while restoring touchpad enable state");
-                }
-
-                // Set longPress event
-                toggleLongPress(context, sInstance, Constants.isPreferenceEnabled(
-                        context, Constants.TOUCHPAD_LONGPRESS_KEY));
-
-                // Set doubleTap event
-                toggleDoubleTap(context, sInstance, Constants.isPreferenceEnabled(
-                        context, Constants.TOUCHPAD_DOUBLETAP_KEY));
-            }
-
             // Disable button settings if needed
             if (!hasButtonProcs()) {
                 disableComponent(context, ButtonSettings.class.getName());
@@ -113,57 +81,7 @@ public class Startup extends BroadcastReceiver {
             if (hasOClick()) {
                 updateOClickServiceState(context);
             }
-        } else if (intent.getAction().equals("lineageos.intent.action.GESTURE_CAMERA")) {
-            long now = SystemClock.uptimeMillis();
-            sendInputEvent(new KeyEvent(now, now, KeyEvent.ACTION_DOWN,
-                    KeyEvent.KEYCODE_CAMERA, 0, 0,
-                    KeyCharacterMap.VIRTUAL_KEYBOARD, 0, 0, InputDevice.SOURCE_KEYBOARD));
-            sendInputEvent(new KeyEvent(now, now, KeyEvent.ACTION_UP,KeyEvent.KEYCODE_CAMERA,
-                    0, 0, KeyCharacterMap.VIRTUAL_KEYBOARD, 0, 0, InputDevice.SOURCE_KEYBOARD));
         }
-    }
-
-    public static void toggleDoubleTap(Context context, IGestureService gestureService,
-            boolean enable) {
-        PendingIntent pendingIntent = null;
-        if (enable) {
-            Intent doubleTapIntent = new Intent("lineageos.intent.action.GESTURE_CAMERA", null);
-            pendingIntent = PendingIntent.getBroadcastAsUser(
-                    context, 0, doubleTapIntent, 0, UserHandle.CURRENT);
-        }
-        try {
-            System.out.println("toggleDoubleTap : " + pendingIntent);
-            gestureService.setOnDoubleClickPendingIntent(pendingIntent);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void toggleLongPress(Context context, IGestureService gestureService,
-            boolean enable) {
-        PendingIntent pendingIntent = null;
-        if (enable) {
-            Intent longPressIntent = new Intent(Intent.ACTION_CAMERA_BUTTON, null);
-            pendingIntent = PendingIntent.getBroadcastAsUser(
-                    context, 0, longPressIntent, 0, UserHandle.CURRENT);
-        }
-        try {
-            System.out.println("toggleLongPress : " + pendingIntent);
-            gestureService.setOnLongPressPendingIntent(pendingIntent);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void sendInputEvent(InputEvent event) {
-        InputManager inputManager = InputManager.getInstance();
-        inputManager.injectInputEvent(event,
-                InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_FINISH);
-    }
-
-    static boolean hasGestureService(Context context) {
-        return !context.getResources().getBoolean(
-                com.android.internal.R.bool.config_enableGestureService);
     }
 
     static boolean hasButtonProcs() {

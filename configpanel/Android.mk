e26b84d2c585e1eb6f7245ef00626553e3a706fa
dianlujitao <dianlujitao@lineageos.org>
2017-11-29 16:59:20 +0800

    oppo-common: ConfigPanel: Re-integrate into Settings
     * Apply the default Oreo theme and inherit the layouts from Google
       for the Settings app, in order to keep UI consistency.
    
     * Get rid of SettingsDrawerActivity as it no longer fits in.
    
     * Add required appcompat and preference support libraries.
    
     * Integrate additional buttons into our custom Button settings
       category by defining the expected action.
    
     * Integrate O-Click into "Connected devices" and set empty summary
       to center.
    
     * Build with AAPT2
    
    Change-Id: I79a227dcc94cc36f2ad7c825401439ba0f0de9ea
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -22,12 +22,15 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := ConfigPanel
+LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v14-preference \
+    android-support-v4 \
     android-support-v7-appcompat \
     android-support-v7-preference \
     android-support-v7-recyclerview \
+    android-support-v13 \
+    android-support-v14-preference \
     org.lineageos.platform.internal
 
 LOCAL_AAPT_FLAGS := \
@@ -39,7 +42,7 @@ LOCAL_AAPT_FLAGS := \
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    $(LOCAL_PATH)/../../../../packages/resources/devicesettings/res \
+    $(TOP)/packages/resources/devicesettings/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/preference/res \
@@ -48,7 +51,5 @@ LOCAL_RESOURCE_DIR := \
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_MODULE_TAGS := optional
-
-include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)

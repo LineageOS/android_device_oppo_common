20f8e2d7cf6434db659b75b224391c1513fafd37
Michael Bestas <mkbestas@lineageos.org>
2018-04-02 00:41:07 +0300

    common: Don't set power HAL variant
    * Devices will set this to their needs
    * Remove device/qcom/common dependency since
      all it contains is the old qcom power HAL
    
    Change-Id: Iee27c33217c907965b33e1caec223945cc38b090

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,9 +20,6 @@ BOARD_VENDOR := oppo
 BOARD_HARDWARE_CLASS += \
     hardware/lineage/lineagehw
 
-# QCOM Power
-TARGET_POWERHAL_VARIANT := qcom
-
 # Releasetools
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_oppo
 TARGET_RELEASETOOLS_EXTENSIONS := device/oppo/common

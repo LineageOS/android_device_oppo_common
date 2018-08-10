82a6848bdb7a1c442f1708e016752d09d9dd9268
YumeMichi <do4suki@gmail.com>
2018-08-10 10:15:11 +0800

    recovery_updater: Update for P compatability
    librecovery_updater_oppo: C_INCLUDES must be under the source or output
    directories: /edify/include /otautil/include /updater/include.
    
    Change-Id: I43216d9a051dd22dfeb926d49b86def7dc3bb4c1
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -6,9 +6,19 @@ ifneq ($(TARGET_PLATFORM_DEVICE_BASE),)
 LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
 
-LOCAL_C_INCLUDES := $(call project-path-for,recovery) \
-    $(call project-path-for,recovery)/updater/include
+LOCAL_C_INCLUDES := \
+    $(TARGET_OUT_HEADERS)/edify \
+    $(TARGET_OUT_HEADERS)/otautil \
+    $(TARGET_OUT_HEADERS)/updater
+
 LOCAL_SRC_FILES := recovery_updater.cpp
+
 LOCAL_MODULE := librecovery_updater_oppo
 LOCAL_MODULE_TAGS := eng
+
+LOCAL_STATIC_LIBRARIES := \
+    libedify \
+    libotautil \
+    libupdater
+
 include $(BUILD_STATIC_LIBRARY)

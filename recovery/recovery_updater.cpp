c7ee976cb6a451ef42d356e8d47467286e1bec1e
YumeMichi <do4suki@gmail.com>
2020-09-21 02:03:11 +0800

    oppo-common: recovery: Update for R compatibility
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: I4ecd1e80989888250ea87e10bb6a252cd9a9e7a7

--- recovery/recovery_updater.cpp
+++ recovery/recovery_updater.cpp
@@ -26,6 +26,8 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <android-base/stringprintf.h>
+
 #include "edify/expr.h"
 #include "otautil/error_code.h"
 #include "updater/install.h"
@@ -180,7 +182,7 @@ Value * VerifyTrustZoneFn(const char *name, State *state, const std::vector<std:
 
     ret = 0;
     for (i = 0; i < argv.size(); i++) {
-        uiPrintf(state, "Comparing TZ version %s to %s",
+        state->updater->UiPrint(android::base::StringPrintf("Comparing TZ version %s to %s",
                 tz_version[i].c_str(), current_tz_version);
         if (strncmp(tz_version[i].c_str(), current_tz_version, tz_version[i].length()) == 0) {
             ret = 1;
